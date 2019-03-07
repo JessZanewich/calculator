b5a3a3ecc01bfa3fe2dbec824cceddfd41571eac
Jessica Zanewich <jzanewich@ca3cmam0xj18ujg.cmass.criticalmass.com>
2019-03-07 15:55:26 -0700

    layout all buttons with correct values in them

--- src/index.js
+++ src/index.js
@@ -1,27 +1,62 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './index.css';
 
 
 class Calculator extends React.Component {
   render() {
     return (
-      <div>Calculator</div>
+      <div className="calculator">
+        <Display />
+        <Buttons />
+      </div>
     );
   }
 }
 
-// class Display extends Component {
+class Display extends Component {
+    render() {
+        return (
+            <div className="display"></div>
+        )
+    }
+}
 
-// }
+class Button extends Component {
+    render() {
+        return (
+            <div className={`button ${this.props.className}`}>
+                {this.props.value}
+            </div>
+        )
+    }
+}
 
-// class Buttons extends Component {
-
-// }
-
-// class Button extends Component {
-  
-// }
+class Buttons extends Component {
+    render() {
+        return (
+            <div className="buttons">
+                <Button value="7" />
+                <Button value="8" />
+                <Button value="9" />
+                <Button value="+" />
+                <Button value="4" />
+                <Button value="5" />
+                <Button value="6" />
+                <Button value="-" />
+                <Button value="1" />
+                <Button value="2" />
+                <Button value="3" />
+                <Button value="x" />
+                <Button value="0" />
+                <Button value="." />
+                <Button value="C" />
+                <Button value="/" />
+                <Button value="=" className="equals" /> 
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
   <Calculator />, 
